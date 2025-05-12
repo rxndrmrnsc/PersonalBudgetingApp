54c8c922df11c2e039002e29720e575d235ccedc
rxndrmrnsc <ruxandraimarinescu@gmail.com>
2025-05-12 14:44:58 +0300

    added dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,33 @@
-import * as React from 'react';
+import { React, useState, useEffect} from "react";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import MonthlyBudget from './MonthlyBudget/MonthlyBudget'
+import Dashboard from './Dashboard/Dashboad'
 
 export default function App() {
+  const mockBudgets = [
+    { id: 1, month: "January", year: 2025 },
+    { id: 2, month: "February", year: 2025 },
+    { id: 3, month: "March", year: 2025 }
+  ];
+
+  const [activeBudgetId, setActiveBudgetId] = useState(null);
+
+  const handleSelectBudget = (id) => {
+    setActiveBudgetId(id);
+  };
+
   return (
     <Container maxWidth={false} sx={{ width: '100%', minWidth: '300px' }}>
-      <MonthlyBudget />
+      {/* <MonthlyBudget /> */}
+      {/* <>
+      {activeBudgetId ? (
+        <BudgetDetail budgetId={activeBudgetId} />
+      ) : ( */}
+      <Dashboard budgets={mockBudgets} onSelectBudget={handleSelectBudget} />
+      {/* )}
+    </> */}
     </Container>
   );
 }
